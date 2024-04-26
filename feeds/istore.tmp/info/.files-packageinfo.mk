7bec9765197ff49e944cbbcaaeb6434ac6589ebd
strayedstar <4bitlearning@gmail.com>
2024-04-26 10:33:48 +0000

    feeds更新,添加istore feed,回退网口更改

---
+++ feeds/istore.tmp/info/.files-packageinfo.mk
@@ -0,0 +1,4 @@
+$(eval $(call PackageDir,luci_luci-app-store,luci/luci-app-store,))
+$(eval $(call PackageDir,luci_luci-lib-taskd,luci/luci-lib-taskd,))
+$(eval $(call PackageDir,luci_luci-lib-xterm,luci/luci-lib-xterm,))
+$(eval $(call PackageDir,luci_taskd,luci/taskd,))
